ae95c8272f46caff0c4b3e26e57980d319729b70
2eg4rizve <2eg4rizve@gmail.com>
2025-08-26 13:23:54 +0600

    folder added

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }       // Primary Key
+        public string Name { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
+
