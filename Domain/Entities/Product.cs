8b31a377800f131a40e0ab6bc471fa0a2ddbf256
2eg4rizve <2eg4rizve@email.com>
2025-12-24 12:41:10 +0600

    update

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -4,6 +4,7 @@ namespace Domain.Entities
     public class Product
     {
         public int Id { get; set; }     
+
         public string Name { get; set; } = string.Empty;
         public string Brand { get; set; } = string.Empty;
         public decimal Price { get; set; }
